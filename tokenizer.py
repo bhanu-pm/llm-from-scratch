270747622719ac6f0e6c786dc5605c0e59130215
bhanu-pm <bhanuprakashm1108@gmail.com>
2024-10-13 14:20:08 -0700

    complete simple tokenizer code

--- tokenizer.py
+++ tokenizer.py
@@ -1,4 +1,5 @@
 import re
+import copy
 
 
 class SimpleTokenizer:
@@ -31,12 +32,30 @@ class SimpleTokenizer:
 		return self.token_ids
 
 	def decoder(self, integers):
-		pass
+		# Getting back the text from token IDs
+		text_list = []
+		for i, val in enumerate(integers):
+			word = self.int_to_str[val]
+			text_list.append(word)
+
+		unparsed_text_list = copy.deepcopy(text_list)
+		unparsed_text_list.remove('<|endoftext|>')
+
+		unparsed_final_text = ' '.join(unparsed_text_list)
+		parsed_final_text = ' '.join(text_list)
+		return unparsed_final_text, parsed_final_text
+
 
 
 if __name__ == '__main__':
-	text = "Thy The thee :;quick & 'brown' fox -jumped!? over, %the \"lazy\" dawgs."
-	tokens_to_int = {'!': 0, '"': 1, '%': 2, '&': 3, "'": 4, ',': 5, '-': 6, '.': 7, ':': 8, ';': 9, '?': 10, 'brown': 11, 'dawgs': 12, 'fox': 13, 'jumped': 14, 'lazy': 15, 'over': 16, 'quick': 17, 'the': 18}
+	text = "Thy The thee :;quick & 'brown' fox -jumps!? over, %the \"lazy\" dawg."
+	tokens_to_int = {'!': 0, '"': 1, '%': 2, '&': 3, "'": 4, ',': 5, '-': 6, '.': 7, ':': 8, ';': 9, '?': 10, 'brown': 11, 'dawg': 12, 'fox': 13, 'jumps': 14, 'lazy': 15, 'over': 16, 'quick': 17, 'the': 18}
 	tokenizer = SimpleTokenizer(tokens_to_int)
 	token_ids_for_text = tokenizer.encoder(text)
+	print("Initial text :")
+	print(text)
 	print(token_ids_for_text)
+
+	rawtext_from_token_ids, text_from_token_ids = tokenizer.decoder(token_ids_for_text)
+	print(rawtext_from_token_ids)
+	print(text_from_token_ids)
